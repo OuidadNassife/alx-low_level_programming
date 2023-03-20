6f69efae19d100a3d2fe608a582ccf245e4b04de
ouidadnassife <nassife.ouidad@gmail.com>
2023-03-19 17:29:28 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,12 +10,15 @@
 int main(void)
 {
 int n = rand() - RAND_MAX / 2;
-if (n > 0)
+if (n >= 0)
 {
-printf("%d is positive \n", n);
 if (n == 0)
 {
 printf("%d is zero \n", n);
+}
+else
+{
+printf("%d is positive \n", n);
 }
 }
 else
