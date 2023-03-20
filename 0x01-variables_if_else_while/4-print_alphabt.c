ae3358807847414649e5cb0aac480230772223c5
ouidadnassife <nassife.ouidad@gmail.com>
2023-03-20 08:03:22 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,19 +6,15 @@
  */
 int main(void)
 {
-        int n = 97;
+int n = 97;
 
-        while(n <= 122)
-        {
-		if(n == 101 || n == 113)
-		{
-			n++;
-			continue;
-		}	
-                putchar(n);
-                n++;
-        }
-        putchar('\n');
-        return (0);
-
+while (n <= 122)
+{
+if (n == 101 || n == 113)
+n++;
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
 }
