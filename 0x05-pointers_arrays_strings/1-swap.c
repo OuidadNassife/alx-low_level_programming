bf1419334dcf45b16d08a673f60e42dc4a34947b
ouidadnassife <nassife.ouidad@gmail.com>
2023-03-27 06:39:50 -0700

    1-main.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -8,30 +8,9 @@
 */
 void swap_int(int *a, int *b)
 {
-		int s;
+int s;
 
-		s = *a;
-		*a = *b;
-		*b = s;
+s = *a;
+*a = *b;
+*b = s;
 }
-
-
-2-strlen.c
-
-#include "main.h"
-/**
- * _strlen - return the length of a string
- * Owned By Bwave
- * @s: char to check
- * Description: this will return the length of a string
- * Return: 0 is success
- */
-int _strlen(char *s)
-{
-	int a = 0;
-
-	for (; *s++;)
-		a++;
-	return (a);
-}
-
